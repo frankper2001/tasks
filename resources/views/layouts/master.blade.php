9340cc201c71612621b065678adcc9e553978ab1
Frank <frankper2001.es@gmail.com>
2021-06-18 13:21:39 +0200

    fix bug in /home

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -54,12 +54,13 @@
                                                 document.getElementById('logout-form').submit();">
                                 {{ __('Logout') }}
                             </a>
-
+                            <a class="dropdown-item" href="/home">Home</a>
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                 @csrf
                             </form>
                         </div>
                     </li>
+                    
                 @endguest
             </ul>
         </div>
